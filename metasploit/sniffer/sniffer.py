7e8ae47bc2d44fee36d6eb6d540fbcef286d1b9c
uahmad417 <18u.ahmad@gmail.com>
2025-07-15 14:30:38 +0300

    created sniffer script

---
+++ metasploit/sniffer/sniffer.py
@@ -0,0 +1,15 @@
+# This script sniffs for network packets
+import sys
+from scapy.all import *
+
+def packet_callback(packet):
+    pass
+
+def packet_filter(packet):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    pass
