9d0a302849c586cde61dfeba222558bfaeff642f
uahmad417 <18u.ahmad@gmail.com>
2025-07-24 20:21:16 +0300

    added comments

--- privelage_escalation/linux/scripts/vulnerable.c
+++ privelage_escalation/linux/scripts/vulnerable.c
@@ -3,5 +3,6 @@
 void main() {
     setuid(1002);
     setgid(1002);
-    system("thm");  // vulnerable use of system()
+    // TODO: Add a command to fit the challange narrative
+    system("thm");
 }
