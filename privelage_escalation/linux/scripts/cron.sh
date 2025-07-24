a797177d9f605c1a01b0b0f4a2d2d681c572c740
uahmad417 <18u.ahmad@gmail.com>
2025-07-24 20:43:47 +0300

    updated cron script to mimic a backup operation

--- privelage_escalation/linux/scripts/cron.sh
+++ privelage_escalation/linux/scripts/cron.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-echo "cron job $(date)" >> /home/user2/cron.log
+BACKUPTIME=`date +%Y-%m-%d_%H:%M:%S`
+DESTINATION="/home/dummyuser/backup_$BACKUPTIME.tar.gz"
+SOURCEFOLDER="/home/dummyuser/Documents/commercial/prices.xlsx"
+tar -cpzf $DESTINATION $SOURCEFOLDER
