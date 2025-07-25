1dd36c22125ae2efaa57a9ef36a23cacc5fda591
uahmad417 <18u.ahmad@gmail.com>
2025-07-25 23:50:25 +0300

    fixed group assignment

--- privelage_escalation/linux/scripts/vulnerable.c
+++ privelage_escalation/linux/scripts/vulnerable.c
@@ -2,7 +2,7 @@
 
 void main() {
     gid_t groups[] = {1002,1004,1005};
-    setgroups(2, groups);
+    setgroups(3, groups);
     setgid(1002);
     setuid(1002);
     // TODO: Add a command to fit the challange narrative
