5cffddb982625c1679fab29766f923a01d5d9520
uahmad417 <18u.ahmad@gmail.com>
2025-07-25 21:57:36 +0300

    set appropriate groups for when the binary is executed

--- privelage_escalation/linux/scripts/vulnerable.c
+++ privelage_escalation/linux/scripts/vulnerable.c
@@ -1,8 +1,10 @@
 #include <unistd.h>
 
 void main() {
-    setuid(1002);
+    gid_t groups[] = {1002,1004,1005};
+    setgroups(2, groups);
     setgid(1002);
+    setuid(1002);
     // TODO: Add a command to fit the challange narrative
     system("thm");
 }
