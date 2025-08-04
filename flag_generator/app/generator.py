e0ad3edb0c15a8178967b107c6fc9a33ec9c6b92
uahmad417 <18u.ahmad@gmail.com>
2025-08-04 08:31:19 +0300

    flag is now gernating by hashing secret and lab labels

--- flag_generator/app/generator.py
+++ flag_generator/app/generator.py
@@ -1,4 +1,7 @@
 from typing import Dict
+import hmac
+import hashlib
+
 
 class FlagGenerator:
     def __init__(self, secret:str, labs: Dict[str, list]):
@@ -9,8 +12,11 @@ class FlagGenerator:
         """
         Generate a flag based on the email, lab name, and task ID.
         """
-        
-        pass
+        base = f"{email}|{lab_name}|{task_id}"
+        digest = hmac.new(self.secret.encode(), base.encode(), hashlib.sha256).hexdigest()
+        truncated_digest = digest[:16]  # Truncate to 16 characters
+        flag = f"FLAG{{{truncated_digest}}}"
+        return flag
     
     def generate_flags(self, email: str):
         """
