87c4eff363bc34c8d2ff2edc1078cdf9085dd80e
uahmad417 <18u.ahmad@gmail.com>
2025-08-04 08:09:45 +0300

    added plugin discovery logic and loading of plugins

--- flag_generator/app/main.py
+++ flag_generator/app/main.py
@@ -2,8 +2,37 @@
 Main script for the Flag Generator application.
 """
 
+import importlib
+import pkgutil
+
+from app import labs
+
+def iter_namespace(ns_pkg):
+    # Specifying the second argument (prefix) to iter_modules makes the
+    # returned name an absolute name instead of a relative one. This allows
+    # import_module to work without having to do additional modification to
+    # the name.
+    return pkgutil.iter_modules(ns_pkg.__path__, ns_pkg.__name__ + ".")
+
+def discover_plugins():
+    """
+    Discover and load plugins for the Flag Generator application.
+    """
+    discovered_plugins = {
+        name: importlib.import_module(name)
+        for finder, name, ispkg
+        in iter_namespace(labs)
+        }
+    return discovered_plugins
+
+
 def main():
-    pass
+    plugins = discover_plugins()
+    print(plugins)
+    for plugin_name, plugin_module in plugins.items():
+        print(f"Loaded plugin: {plugin_name}")
+        if hasattr(plugin_module, 'group_tasks'):
+            plugin_module.group_tasks()
 
 if __name__ == "__main__":
     main()
