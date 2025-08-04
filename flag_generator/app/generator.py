c01f814db5fd51b7679f235ecff78f99a32dbb36
uahmad417 <18u.ahmad@gmail.com>
2025-08-04 08:38:42 +0300

    flags for all labs and tasks are generated dynamically once

--- flag_generator/app/generator.py
+++ flag_generator/app/generator.py
@@ -22,4 +22,11 @@ class FlagGenerator:
         """
         Generate flags for the given email based on the secret and labs.
         """
-        pass
+        
+        flags = {}
+        for lab_name, tasks in self.labs.items():
+            flags.update({lab_name: []})
+            for task_id in tasks:
+                flag = self.__make_flag(email, lab_name, task_id)
+                flags[lab_name].append((task_id, flag))
+        return flags
