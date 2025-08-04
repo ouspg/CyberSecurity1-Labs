a468d5fdb10e85104bef2fbdbffe96ebe66a3b68
uahmad417 <18u.ahmad@gmail.com>
2025-08-04 06:41:18 +0300

    created labinjector class which represenets all tasks part of the lab

--- flag_generator/src/injector.py
+++ flag_generator/src/injector.py
@@ -7,8 +7,29 @@ class TaskInjector(ABC):
     """
 
     @abstractmethod
-    def inject(self, task):
+    def inject(self, flag: str):
         """
         Inject flags for a task.
         """
         pass
+
+class LabInjector:
+    """
+    Class for grouping multiple task injectors.
+    """
+
+    def __init__(self, tasks: list[TaskInjector]):
+        self.injectors = tasks
+
+    def add_injector(self, injector: TaskInjector):
+        """
+        Add a task injector to the lab.
+        """
+        self.injectors.append(injector)
+
+    def inject_all(self, flag: str):
+        """
+        Inject flags for all task injectors in the lab.
+        """
+        for injector in self.injectors:
+            injector.inject(flag)
