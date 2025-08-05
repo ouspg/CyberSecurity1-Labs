3a2bd1ea0d8d897155d57e426345eadf3d25b94a
uahmad417 <18u.ahmad@gmail.com>
2025-08-05 20:29:15 +0300

    moved the plugins discovery method to utils

--- flag_generator/app/utils.py
+++ flag_generator/app/utils.py
@@ -3,3 +3,34 @@ This module contains utility and helper methods for the
 Flag Generator Application.
 """
 
+import importlib
+import pkgutil
+
+
+def iter_namespace(ns_pkg):
+    # Specifying the second argument (prefix) to iter_modules makes the
+    # returned name an absolute name instead of a relative one. This allows
+    # import_module to work without having to do additional modification to
+    # the name.
+
+    return pkgutil.iter_modules(ns_pkg.__path__, ns_pkg.__name__ + ".")
+
+
+def discover_plugins(ns_pkg):
+    """
+    Discover and load plugins for the Flag Generator application.
+    This function iterates through the labs package and imports all modules
+    that are defined as plugins. It returns a dictionary where the keys are
+    the plugin names and the values are the corresponding module objects.
+
+    Returns:
+        Dict[str, module]: A dictionary of discovered plugins where the keys are
+            plugin names and the values are the corresponding module objects.
+    """
+
+    discovered_plugins = {
+        name: importlib.import_module(name)
+        for finder, name, ispkg
+        in iter_namespace(ns_pkg)
+    }
+    return discovered_plugins
