1c3fe831ed38199d41f30c51568e31fde80bd4c1
uahmad417 <18u.ahmad@gmail.com>
2025-08-05 20:37:33 +0300

    updated docstrings

--- flag_generator/app/utils.py
+++ flag_generator/app/utils.py
@@ -5,9 +5,10 @@ Flag Generator Application.
 
 import importlib
 import pkgutil
+from typing import Dict, Iterator
 
 
-def iter_namespace(ns_pkg):
+def iter_namespace(ns_pkg) -> Iterator[pkgutil.ModuleInfo]:
     # Specifying the second argument (prefix) to iter_modules makes the
     # returned name an absolute name instead of a relative one. This allows
     # import_module to work without having to do additional modification to
@@ -16,7 +17,7 @@ def iter_namespace(ns_pkg):
     return pkgutil.iter_modules(ns_pkg.__path__, ns_pkg.__name__ + ".")
 
 
-def discover_plugins(ns_pkg):
+def discover_plugins(ns_pkg) -> Dict[str, pkgutil.ModuleInfo]:
     """
     Discover and load plugins for the Flag Generator application.
     This function iterates through the labs package and imports all modules
@@ -24,7 +25,7 @@ def discover_plugins(ns_pkg):
     the plugin names and the values are the corresponding module objects.
 
     Returns:
-        Dict[str, module]: A dictionary of discovered plugins where the keys are
+        Dict[str, pkgutil.ModuleInfo]: A dictionary of discovered plugins where the keys are
             plugin names and the values are the corresponding module objects.
     """
 
