cd76070dfc4f5c7cd176ecdfcd05c394a19b89b8
uahmad417 <18u.ahmad@gmail.com>
2025-08-07 04:06:49 +0300

    added injection logic for task 4

--- flag_generator/app/labs/metasploit/__init__.py
+++ flag_generator/app/labs/metasploit/__init__.py
@@ -7,6 +7,7 @@ from app.injector import Lab, Task
 from app.utils.config import get_config
 import docker
 from app.utils.logger import setup_logger
+import os
 
 logger = setup_logger(__name__)
 
@@ -68,6 +69,39 @@ class TaskThree(Task):
         container = client.containers.get(get_config("task_three", "container_name"))
         container.exec_run(
             f"sh -c 'echo {self.get_flag()} > {get_config("task_three", "flag_location")}'")
+        
+class TaskFour(Task):
+    """
+    Second task for the lab.
+    This task invovles identifying the CVE that allows remote code execution
+    into the target.
+    """
+
+    def __init__(self, task_id: str):
+        super().init(task_id)
+    
+    def inject(self):
+        """
+        Inject the flag for this task.
+        Since the service name/version is static, there is no actual injection logic
+        involved
+        """
+
+        # replace embeded flag in the binary
+        with open(get_config("task_four", "sniffer_location"), "r") as f:
+            content = f.read()
+        
+        content = content.replace(get_config("task_four", "palceholder_flag"), self.get_flag())
+
+        with open(get_config("task_four", "sniffer_location"), "r") as f:
+            f.write(content)
+        
+        # build the container again
+        os.system(f"docker compose up -f {get_config('task_four', 'compose_file_location')} -d --build")
+
+
+        
+           
 
 
         
