acc0f9e0efe30df1b4a9ced345acc6ddfa2ced25
uahmad417 <18u.ahmad@gmail.com>
2025-08-07 03:27:39 +0300

    made it mandotory to provide config sections and environment variable for configs

--- flag_generator/app/utils/config.py
+++ flag_generator/app/utils/config.py
@@ -26,16 +26,16 @@ def load_configs(config_files: List[str]):
         CONFIG.read(file)
 
 
-def get_config(section: str = "", key: str = "", env_var: str = "") -> str | None:
+def get_config(section: str, key: str, env_var: str) -> str | None:
     """
     Read the config option from the config file.
     The configs are first read from the config file. If the config section and key
-    are not provided then it tries to read it from the environemnt.
+    do not exist then it tries to read it from the environemnt.
 
     Parameters:
-        section (str): Option config file section
-        key (str): Optional config file key
-        env_var (str): Optional environment variable
+        section (str): config file section
+        key (str): config file key
+        env_var (str): environment variable
 
     Returns:
         (str | None): The config value or None if no config sepcified
@@ -43,9 +43,9 @@ def get_config(section: str = "", key: str = "", env_var: str = "") -> str | Non
 
     config_value = None
 
-    if section and key:
+    try:
         config_value = CONFIG[section][key]
-    else:
+    except KeyError:
         config_value = os.environ.get(env_var)
 
     return config_value
