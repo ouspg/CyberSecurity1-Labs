99fa9dbdda3712ed3e9fa1812111479b4c9b6bd0
uahmad417 <18u.ahmad@gmail.com>
2025-08-19 01:14:16 +0300

    fixed typo

--- packer/linux/scripts/firstboot.sh
+++ packer/linux/scripts/firstboot.sh
@@ -39,7 +39,7 @@ getStudentEmail() {
     # Run the loop until a valid email is entered
     while [ $exitstatus != 0 ]
     do
-        STUDENT_EMAIL=$(whiptail --inputbox "Enter your email address:" 8 39 student@studnent.oulu.fi --title "Labs Setup" 3>&1 1>&2 2>&3)
+        STUDENT_EMAIL=$(whiptail --inputbox "Enter your email address:" 8 39 student@student.oulu.fi --title "Labs Setup" 3>&1 1>&2 2>&3)
         # A trick to swap stdout and stderr.
         # Again, you can pack this inside if, but it seems really long for some 80-col terminal users.
         exitstatus=$?
