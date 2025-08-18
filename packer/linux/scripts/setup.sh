6e0431ac611eeebc567f0282ef4f3a25137cae69
uahmad417 <18u.ahmad@gmail.com>
2025-08-18 20:54:04 +0300

    setup python environment in the vm

--- packer/linux/scripts/setup.sh
+++ packer/linux/scripts/setup.sh
@@ -19,6 +19,8 @@ apt-get update
 
 apt-get install docker-ce -y docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+# installing python
+apt-get install -y python3 python3-pip python3-venv
 # Setup directories for labs
 mkdir -p /labs/vuln_research /labs/metasploit /labs/priv_esc /labs/web
 
@@ -29,3 +31,9 @@ mv /tmp/firstboot.service /etc/systemd/system/firstboot.service
 mv /tmp/firstboot.sh /usr/local/bin/firstboot.sh
 chmod +x /usr/local/bin/firstboot.sh
 systemctl enable firstboot.service
+
+# setup flag generator
+mv /tmp/app/ /usr/lib/python3.12/app
+python3 -m venv /opt/venv
+source /opt/venv/bin/activate
+pip3 install -r /usr/lib/python3.12/app/requirements.txt
