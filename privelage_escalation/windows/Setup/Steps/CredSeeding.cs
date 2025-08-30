face4146facd3dacb81a11c5f9bb3da24a68b23a
uahmad417 <18u.ahmad@gmail.com>
2025-08-30 17:14:55 +0300

    added password to ps history and saved user creds

--- privelage_escalation/windows/Setup/Steps/CredSeeding.cs
+++ privelage_escalation/windows/Setup/Steps/CredSeeding.cs
@@ -1,9 +1,18 @@
+using Setup.Common;
+
 namespace Setup.Steps;
 
 public static class CredSeeding
 {
-    public static void Run()
+    public static void Run(Context ctx)
     {
+        // Password in PS history
         Console.WriteLine("Seeding Credentials");
+        var hist = $@"C:\Users\{ctx.StudentUser}\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadline\ConsoleHost_history.txt";
+        Directory.CreateDirectory(Path.GetDirectoryName(hist)!);
+        File.AppendAllText(hist, $"cmdkey /add:LAB-PC /user:{ctx.StudentUser} /pass:{ctx.StudentUserPass}");
+
+        // Saved credentials
+        Shell.Run($"runas /user:{ctx.DevUser} /pass:{ctx.DevUserPass} 'cmdkey /add:LAB-PC /user:{ctx.SvcUser} /pass:{ctx.SvcUserPass}'")
     }
 }
