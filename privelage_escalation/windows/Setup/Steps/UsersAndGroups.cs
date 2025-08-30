f72adc0135bde510e8b0cf3083e5f9f0f7a1ea7d
uahmad417 <18u.ahmad@gmail.com>
2025-08-30 15:53:19 +0300

    added method to create user accounts

--- privelage_escalation/windows/Setup/Steps/UsersAndGroups.cs
+++ privelage_escalation/windows/Setup/Steps/UsersAndGroups.cs
@@ -1,9 +1,17 @@
+using Setup.Common;
+
 namespace Setup.Steps;
 
 public static class UsersAndGroups
 {
-    public static void Run()
+    public static void Run(Context ctx)
     {
         Console.WriteLine("Creating Users and Groups");
+        CreateLocalAccount(ctx.StudentUser, ctx.StudentUserPass);
+    }
+
+    static void CreateLocalAccount(string username, string password)
+    {
+        Shell.Run("pwd");
     }
 }
