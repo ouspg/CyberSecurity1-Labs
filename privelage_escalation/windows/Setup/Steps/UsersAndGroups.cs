431e793787142c02ed16e7646680ececdefa8b15
uahmad417 <18u.ahmad@gmail.com>
2025-08-30 16:59:28 +0300

    added shell command to create local user accounts

--- privelage_escalation/windows/Setup/Steps/UsersAndGroups.cs
+++ privelage_escalation/windows/Setup/Steps/UsersAndGroups.cs
@@ -8,10 +8,20 @@ public static class UsersAndGroups
     {
         Console.WriteLine("Creating Users and Groups");
         CreateLocalAccount(ctx.StudentUser, ctx.StudentUserPass);
+        CreateLocalAccount(ctx.DevUser, ctx.DevUserPass);
+        CreateLocalAccount(ctx.SvcUser, ctx.SvcUserPass);
+        CreateLocalAccount(ctx.AdminUser, ctx.AdminUserUserPass);
+
+        // Make adminlab a local admin
+        Shell.Run($"Add-LocalGroupMember -Group 'Administrators' -Member '{ctx.AdminUser}' -ErrorAction SilentlyContinue");
     }
 
     static void CreateLocalAccount(string username, string password)
     {
-        Shell.Run("pwd");
+        Shell.Run($@"
+            if (-not (Get-LocalUser -Name '{username}' -ErrorAction SilentlyContinue)) {{
+                $p = ConvertTo-SecureString '{password}' -AsPlainText -Force
+                New-LocalUser -Name '{username}' -Password $p -NoPasswordExpired -PasswordNeverExpires
+            }}");
     }
 }
