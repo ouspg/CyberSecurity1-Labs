ea12bb30ec2a64fce6e6d47397871f996647f764
uahmad417 <18u.ahmad@gmail.com>
2025-08-30 18:02:20 +0300

    added service info replaced environment variables with hardocded values as envrion variables would be diffult to pass during proviosioning

--- privelage_escalation/windows/Setup/Common/Context.cs
+++ privelage_escalation/windows/Setup/Common/Context.cs
@@ -5,12 +5,15 @@ public sealed class Context
     public string StudentUser = "student";
     public string DevUser = "Dev";
     public string SvcUser = "SVC";
-    public string AdminLab = "Admin Lab";
+    public string AdminUser= "Admin Lab";
 
-    public string StudentUserPass = Environment.GetEnvironmentVariable("ACCOUNT_A_PASS");
-    public string DevUserPass = Environment.GetEnvironmentVariable("ACCOUNT_B_PASS");
-    public string SvcUserPass = Environment.GetEnvironmentVariable("ACCOUNT_C_PASS");
-    public string AdminLabUserPass = Environment.GetEnvironmentVariable("ACCOUNT_D_PASS");
+    public string StudentUserPass = "password1";
+    public string DevUserPass = "password1";
+    public string SvcUserPass = "password1";
+    public string AdminUserUserPass = "password1";
+
+    public string ServiceName = "Acme Tools";
+    public string ServiceDisplayName = "Acme Tools Service";
 
     public static Context LoadOrCreate()
     {
