6af7f4f684020d880c472da54652642f4d9d80b7
uahmad417 <18u.ahmad@gmail.com>
2025-08-31 16:49:16 +0300

    added app entry point

--- privelage_escalation/windows/Setup/Program.cs
+++ privelage_escalation/windows/Setup/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace Setup;
+﻿using Setup.Common;
+using Setup.Steps;
+
+namespace Setup;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var ctx = Context.LoadOrCreate();
+        UsersAndGroups.Run(ctx);
+        CredSeeding.Run(ctx);
+        // Services.Run();
+        // Privelages.Run();
+
     }
 }
